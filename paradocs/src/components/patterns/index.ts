341df8fb81f5f98ff0ba975c3bf35d969d872b6b
eq1725 <158111169+eq1725@users.noreply.github.com>
2026-01-27 15:35:04 -0600

    Create index.ts

---
+++ paradocs/src/components/patterns/index.ts
@@ -0,0 +1,4 @@
+export { default as PatternCard } from './PatternCard'
+export type { Pattern } from './PatternCard'
+export { default as TrendingPatternsWidget } from './TrendingPatternsWidget'
+export { default as InsightNarrative } from './InsightNarrative'
