f5afa829b3048b19c8ce4190af3afa98291e8e66
eq1725 <williamschaseh@gmail.com>
2026-01-29 13:45:08 +0000

    Trigger rebuild after reconnecting GitHub

--- next.config.js
+++ next.config.js
@@ -16,3 +16,4 @@ module.exports = nextConfig
 // Build trigger: Thu Jan 29 13:06:49 UTC 2026
 // Deployment trigger 1769692729
 // Build trigger: Thu Jan 29 13:25:23 UTC 2026
+// Rebuild trigger: 1769694308
