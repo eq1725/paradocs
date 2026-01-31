76bbf08da6a48db3d28c174c88b623bd425f3d48
eq1725 <158111169+eq1725@users.noreply.github.com>
2026-01-31 10:16:51 -0600

    Add favicon meta tags to _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+              <link rel="icon" href="/favicon.ico" sizes="any" />
+              <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+              <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
